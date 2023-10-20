bb6d1745a100ae743123b17295b75dcbf91c254c
yogii006 <128319693+yogii006@users.noreply.github.com>
2023-10-21 00:34:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 st.header("Projects by Yogesh Yadav on Machine learning")
-col1, col2, col3= st.columns(3)
+col1, col2 = st.columns(2)
 image_width = 200  # Set the width you want
 with col1:
     st.image("ipl.webp", use_column_width=False, width=image_width)
@@ -8,6 +8,10 @@ with col1:
 with col2:
     st.image("heart.png", use_column_width=False, width=image_width)
     st.markdown("### [Heart disease prediction](https://yogii006heart-disease.streamlit.app/)")
+col3,col4 = st.column(2
 with col3:
    st.image("bank.jpeg", use_column_width=False, width=image_width)
    st.markdown("### [Loan approval prediction](https://yogii006loan.streamlit.app/)")
+with col4:
+   st.image("bank.jpeg", use_column_width=False, width=image_width)
+   st.markdown("### [Loan approval prediction](https://yogii006.streamlit.app/)")
