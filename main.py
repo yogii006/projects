ab008a6afb82b8a6f74c96ef2409481d848a949b
yogii006 <128319693+yogii006@users.noreply.github.com>
2023-10-21 00:39:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ with col1:
 with col2:
     st.image("heart.png", use_column_width=False, width=image_width)
     st.markdown("### [Heart disease prediction](https://yogii006heart-disease.streamlit.app/)")
-col3,col4 = st.columns(2)
+col3, col4 = st.columns(2)
 with col3:
    st.image("bank.jpeg", use_column_width=False, width=image_width)
    st.markdown("### [Loan approval prediction](https://yogii006loan.streamlit.app/)")
