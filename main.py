be72ef986ba63ec1cc97a116a4a470e51c5cada3
Yogesh Yadav <128319693+yogii006@users.noreply.github.com>
2024-01-24 18:43:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.header("Projects by Yogesh Yadav on Machine learning")
 col1, col2,col5 = st.columns(3)
-image_width = 300  # Set the width you want
+image_width = 250  # Set the width you want
 with col1:
     st.image("bank.jpeg", use_column_width=False, width=image_width)
     st.markdown("### [IPL win prediction](https://yogii006ipl.streamlit.app/)")
