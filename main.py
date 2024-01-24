eaf3018db7fea3d3e2b63f1eefab2393e39ea6d8
Yogesh Yadav <128319693+yogii006@users.noreply.github.com>
2024-01-24 18:42:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 st.header("Projects by Yogesh Yadav on Machine learning")
-col1, col2 = st.columns(2)
+col1, col2,col5 = st.columns(2)
 image_width = 300  # Set the width you want
 with col1:
     st.image("bank.jpeg", use_column_width=False, width=image_width)
@@ -10,17 +10,12 @@ with col2:
     st.image("bank.jpeg", use_column_width=False, width=image_width)
     st.markdown("### [Heart disease prediction](https://yogii006heart-disease.streamlit.app/)")
     st.markdown("### [Github code](https://github.com/yogii006/heart-disease)")
-col5, col6 = st.columns(2)
-image_width = 300  # Set the width you want
 with col5:
     st.image("ipl.jpg", use_column_width=False, width=image_width)
     st.markdown("### [IPL win prediction](https://yogii006ipl.streamlit.app/)")
     st.markdown("### [Github code](https://github.com/yogii006/ipl)")
-with col6:
-    st.image("heart.png", use_column_width=False, width=image_width)
-    st.markdown("### [Heart disease prediction](https://yogii006heart-disease.streamlit.app/)")
-    st.markdown("### [Github code](https://github.com/yogii006/heart-disease)")
-col3, col4 = st.columns(2)
+
+col3, col4,col6 = st.columns(2)
 with col3:
    st.image("bank.jpeg", use_column_width=False, width=image_width)
    st.markdown("### [Loan approval prediction](https://yogii006loan.streamlit.app/)")
@@ -28,3 +23,7 @@ with col3:
 with col4:
    st.image("project.jpeg", use_column_width=False, width=image_width)
    st.markdown("### [Github code For this project](https://github.com/yogii006/projects)")
+with col6:
+    st.image("heart.png", use_column_width=False, width=image_width)
+    st.markdown("### [Heart disease prediction](https://yogii006heart-disease.streamlit.app/)")
+    st.markdown("### [Github code](https://github.com/yogii006/heart-disease)")
