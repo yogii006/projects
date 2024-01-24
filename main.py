87d5dfed9bcc9dc19d5f73924b7d9c31258e1b70
Yogesh Yadav <128319693+yogii006@users.noreply.github.com>
2024-01-24 18:40:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,20 @@ st.header("Projects by Yogesh Yadav on Machine learning")
 col1, col2 = st.columns(2)
 image_width = 300  # Set the width you want
 with col1:
-    st.image("ipl.jpg", use_column_width=False, width=image_width)
+    st.image("bank.jpeg", use_column_width=False, width=image_width)
     st.markdown("### [IPL win prediction](https://yogii006ipl.streamlit.app/)")
     st.markdown("### [Github code](https://github.com/yogii006/ipl)")
 with col2:
+    st.image("bank.jpeg", use_column_width=False, width=image_width)
+    st.markdown("### [Heart disease prediction](https://yogii006heart-disease.streamlit.app/)")
+    st.markdown("### [Github code](https://github.com/yogii006/heart-disease)")
+col5, col6 = st.columns(2)
+image_width = 300  # Set the width you want
+with col5:
+    st.image("ipl.jpg", use_column_width=False, width=image_width)
+    st.markdown("### [IPL win prediction](https://yogii006ipl.streamlit.app/)")
+    st.markdown("### [Github code](https://github.com/yogii006/ipl)")
+with col6:
     st.image("heart.png", use_column_width=False, width=image_width)
     st.markdown("### [Heart disease prediction](https://yogii006heart-disease.streamlit.app/)")
     st.markdown("### [Github code](https://github.com/yogii006/heart-disease)")
